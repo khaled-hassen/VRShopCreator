a1be8ced57c88afabbaf4e78b211ebfe65deff3b
khaled <khaledhassen@proton.me>
2024-02-19 21:22:05 +0100

    setup unity project

--- Source/StoreCreator/GameMode/StoreCreatorGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "StoreCreatorGameMode.h"
-#include "UObject/ConstructorHelpers.h"
-
-AStoreCreatorGameMode::AStoreCreatorGameMode()
-	: Super()
-{
-}
