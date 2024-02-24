d23a68bae3e7795f39c56c1304e0672ce21a7d26
khaled <khaledhassen@proton.me>
2024-02-24 16:19:00 +0100

    add direct and distance grabbing, disable the movement using the controller that's grabbing and object

---
+++ Assets/Player/Controllers/Scripts/GrabBehaviorManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Player.Controllers.Scripts
+{
+    public class GrabBehaviorManager : MonoBehaviour
+    {
+        [SerializeField] private TeleportationProvider teleportationProvider;
+        [SerializeField] private TeleportationManager teleportationManager;
+        [SerializeField] private ActionBasedController teleportationController;
+
+        [SerializeField] private ActionBasedContinuousMoveProvider continuousMoveProvider;
+        [SerializeField] private ActionBasedContinuousTurnProvider continuousTurnProvider;
+
+        [SerializeField] private XRDirectInteractor leftDirectInteractor;
+        [SerializeField] private XRRayInteractor leftRayInteractor;
+
+        [SerializeField] private XRDirectInteractor rightDirectInteractor;
+        [SerializeField] private XRRayInteractor rightRayInteractor;
+
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            leftDirectInteractor.selectEntered.AddListener(OnLeftGrabEntered);
+            leftDirectInteractor.selectExited.AddListener(OnLeftGrabExited);
+            leftRayInteractor.selectEntered.AddListener(OnLeftGrabEntered);
+            leftRayInteractor.selectExited.AddListener(OnLeftGrabExited);
+
+            rightDirectInteractor.selectEntered.AddListener(OnRightGrabEntered);
+            rightDirectInteractor.selectExited.AddListener(OnRightGrabExited);
+            rightRayInteractor.selectEntered.AddListener(OnRightGrabEntered);
+            rightRayInteractor.selectExited.AddListener(OnRightGrabExited);
+        }
+
+        private void OnLeftGrabEntered(SelectEnterEventArgs args)
+        {
+            continuousMoveProvider.enabled = false;
+            continuousTurnProvider.enabled = false;
+        }
+
+        private void OnLeftGrabExited(SelectExitEventArgs args)
+        {
+            continuousMoveProvider.enabled = true;
+            continuousTurnProvider.enabled = true;
+        }
+
+        private void OnRightGrabEntered(SelectEnterEventArgs args)
+        {
+            teleportationProvider.enabled = false;
+            teleportationController.enabled = false;
+            teleportationManager.IsTeleportationDisabled = true;
+        }
+
+        private void OnRightGrabExited(SelectExitEventArgs args)
+        {
+            teleportationProvider.enabled = true;
+            teleportationController.enabled = true;
+            teleportationManager.IsTeleportationDisabled = false;
+        }
+    }
+}
--- Assets/Player/Controllers/Scripts/TeleportationManager.cs
+++ Assets/Player/Controllers/Scripts/TeleportationManager.cs
@@ -2,55 +2,62 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class TeleportationManager : MonoBehaviour
+namespace Player.Controllers.Scripts
 {
-    [SerializeField] private InputActionProperty activateAction;
-    [SerializeField] private InputActionProperty cancelAction;
-    [SerializeField] private XRRayInteractor rayInteractor;
-    [SerializeField] private TeleportationProvider teleportationProvider;
-
-    private bool _isTeleporting;
-    private XRInteractorLineVisual _rayInteractorLineVisual;
-
-
-    // Start is called before the first frame update
-    private void Start()
+    public class TeleportationManager : MonoBehaviour
     {
-        _rayInteractorLineVisual = rayInteractor.GetComponent<XRInteractorLineVisual>();
+        [SerializeField] private InputActionProperty activateAction;
+        [SerializeField] private InputActionProperty cancelAction;
+        [SerializeField] private XRRayInteractor rayInteractor;
+        [SerializeField] private TeleportationProvider teleportationProvider;
 
-        activateAction.action.Enable();
-        activateAction.action.performed += OnTeleportActivate;
+        private bool _isTeleporting;
+        private XRInteractorLineVisual _rayInteractorLineVisual;
 
-        cancelAction.action.Enable();
-        cancelAction.action.performed += OnTeleportCancel;
+        public bool IsTeleportationDisabled { get; set; }
 
-        teleportationProvider.endLocomotion += OnTeleportationEnded;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _rayInteractorLineVisual = rayInteractor.GetComponent<XRInteractorLineVisual>();
 
-        ToggleRayInteractor(false);
-    }
+            activateAction.action.Enable();
+            activateAction.action.performed += OnTeleportActivate;
 
-    private void ToggleRayInteractor(bool value)
-    {
-        rayInteractor.enabled = value;
-        _rayInteractorLineVisual.enabled = value;
-    }
+            cancelAction.action.Enable();
+            cancelAction.action.performed += OnTeleportCancel;
 
-    private void OnTeleportActivate(InputAction.CallbackContext context)
-    {
-        ToggleRayInteractor(true);
-        _isTeleporting = true;
-    }
+            teleportationProvider.endLocomotion += OnTeleportationEnded;
 
-    private void OnTeleportCancel(InputAction.CallbackContext context)
-    {
-        if (!_isTeleporting) return;
-        ToggleRayInteractor(false);
-        _isTeleporting = false;
-    }
+            ToggleRayInteractor(false);
+        }
 
-    private void OnTeleportationEnded(LocomotionSystem _)
-    {
-        _isTeleporting = false;
-        ToggleRayInteractor(false);
+        private void ToggleRayInteractor(bool value)
+        {
+            rayInteractor.enabled = value;
+            _rayInteractorLineVisual.enabled = value;
+        }
+
+        private void OnTeleportActivate(InputAction.CallbackContext ctx)
+        {
+            if (IsTeleportationDisabled) return;
+            ToggleRayInteractor(true);
+            _isTeleporting = true;
+        }
+
+        private void OnTeleportCancel(InputAction.CallbackContext ctx)
+        {
+            if (IsTeleportationDisabled) return;
+            if (!_isTeleporting) return;
+            ToggleRayInteractor(false);
+            _isTeleporting = false;
+        }
+
+        private void OnTeleportationEnded(LocomotionSystem system)
+        {
+            if (IsTeleportationDisabled) return;
+            _isTeleporting = false;
+            ToggleRayInteractor(false);
+        }
     }
 }
