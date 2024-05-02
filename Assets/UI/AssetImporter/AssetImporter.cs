9dd66e87888c9021dcf2f922a0a684f7f8e400e9
khaled <khaledhassen@proton.me>
2024-05-02 02:14:43 +0200

    display filename in the assetName input

--- Assets/UI/AssetImporter/AssetImporter.cs
+++ Assets/UI/AssetImporter/AssetImporter.cs
@@ -1,14 +1,24 @@
+using System.IO;
+using TMPro;
+using UnityEngine;
+
 namespace UI.AssetImporter
 {
     public abstract class AssetImporter : UIScreen
     {
         public delegate void OnTabClickedCallback(string filePath);
 
+        [SerializeField] private TMP_InputField inputField;
+
         protected string _filePath;
         public event OnTabClickedCallback OnTabClicked;
 
         public void OnTabClickedEvent() => OnTabClicked?.Invoke(_filePath);
 
-        public virtual void LoadUI(string filePath) => _filePath = filePath;
+        public void LoadUI(string filePath)
+        {
+            _filePath = filePath;
+            inputField.text = Path.GetFileNameWithoutExtension(filePath);
+        }
     }
 }
